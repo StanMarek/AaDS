8d788022cb047c1f6d1ef45418c2c3000d5266d2
StanMarek <stanislawmarek21@gmail.com>
2020-12-19 12:45:16 +0100

    Start Java project, create LL (working)

---
+++ Java/src/Main.java
@@ -0,0 +1,11 @@
+import linkedList.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.add(5);
+        ll.add(10);
+        ll.show();
+    }
+}
---
+++ Java/src/linkedList/LinkedList.java
@@ -0,0 +1,28 @@
+package linkedList;
+
+public class LinkedList {
+    private Node head;
+
+    public LinkedList(){
+        head = null;
+    }
+
+    public void add(int x){
+        Node newNode = new Node();
+        newNode.val = x;
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void show(){
+        Node tmp = head;
+        System.out.print("Head->");
+        while(tmp != null){
+            System.out.print(tmp.val);
+            System.out.print("->");
+            tmp = tmp.next;
+        }
+        System.out.println("NULL");
+    }
+
+}
