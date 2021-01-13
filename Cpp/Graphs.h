f10fcd87ce57b739c1dd724cc46085c55b8bbebe
StanMarek <stanislawmarek21@gmail.com>
2021-01-13 21:13:23 +0100

    Add prim algorithm

--- Cpp/Graphs.h
+++ Cpp/Graphs.h
@@ -3,45 +3,45 @@
 
 namespace gr {
 	
-	int** createMatrix(int& rozmiar) {
+	int** createMatrix(int& size) {
 		std::fstream read;
 		read.open("graf.txt");
-		read >> rozmiar;
+		read >> size;
 
-		int** matrix = new int* [rozmiar];
-		for (int i = 0; i < rozmiar; i++)
-			matrix[i] = new int[rozmiar];
+		int** matrix = new int* [size];
+		for (int i = 0; i < size; i++)
+			matrix[i] = new int[size];
 
-		for (int i = 0; i < rozmiar; i++) {
-			for (int j = 0; j < rozmiar; j++)
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++)
 				read >> matrix[i][j];
 		}
 
 		return matrix;
 	}
 
-	struct Node {
+	struct NodeNeigh {
 		int to;
 		int dis; //distance
-		Node* ng; //neighbour
+		NodeNeigh* ng; //neighbour
 	};
-	Node** createLN(int& rozmiar) {
+	NodeNeigh** createListOfNeighbourhood(int& size) {
 		std::fstream read;
 		read.open("graf.txt");
-		read >> rozmiar;
+		read >> size;
 
-		Node** LN = new Node * [rozmiar];
-		for (int i = 0; i < rozmiar; i++)
+		NodeNeigh** LN = new NodeNeigh * [size];
+		for (int i = 0; i < size; i++)
 			LN[i] = nullptr;
 
 		int currentVal;
 
-		for (int i = 0; i < rozmiar; i++) {
-			for (int j = 0; j < rozmiar; j++) {
-				Node* tmp = LN[i];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				NodeNeigh* tmp = LN[i];
 				read >> currentVal;
 				if (currentVal != 0) {
-					LN[i] = new Node;
+					LN[i] = new NodeNeigh;
 					LN[i]->dis = currentVal;
 					LN[i]->to = j;
 					LN[i]->ng = tmp;
@@ -51,14 +51,115 @@ namespace gr {
 
 		return LN;
 	}
-	void printLn(Node* list){
-		
-	}
 
-	struct Node2 {
+	struct NodeEdge {
 		int to;
 		int from;
 		int dis;
-		Node* ng;
+		NodeEdge* ng;
 	};
+	NodeEdge* createListOfEdges(int& size) {
+		std::fstream read;
+		read.open("graf.txt");
+		read >> size;
+
+		NodeEdge* LE = nullptr;
+
+		int currentVal;
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				read >> currentVal;
+				if (currentVal != 0) {
+					NodeEdge* tmp2 = LE;
+					LE = new NodeEdge;
+					LE->dis = currentVal;
+					LE->to = j;
+					LE->ng = tmp2;
+					LE->from = i;
+				}
+			}
+		}
+		return LE;
+	}
+	bool everyNodeVisited(bool* array, int size) {
+		for (int i = 0; i < size; i++) {
+			if (array[i] == true)
+				return true;
+		}
+		return false;
+	}
+	void AddEdgeNode(NodeEdge*& head, int from, int to, int dis) {
+		NodeEdge* p = new NodeEdge;
+		p->from = from;
+		p->to = to;
+		p->dis = dis;
+		p->ng = head;
+		head = p;
+	}
+	NodeEdge* PrimsAlgorithm(NodeNeigh** ln, int& size, int start) {
+		
+		NodeEdge* outcome = nullptr;
+		bool* visited = new bool[size];
+		for (int i = 0; i < size; i++)
+			visited[i] = false;
+		visited[start] = true;
+
+		int minimum = -1;
+		int startEdge;
+		int endEdge;
+		
+		do {
+			for (int i = 0; i < size; i++) {
+				if (visited[i] == false) {
+					NodeNeigh* newNode = ln[i];
+					while (newNode){
+						if (minimum == -1 && visited[newNode->to] == true) {
+							minimum = newNode->dis;
+							endEdge = newNode->to;
+							startEdge = i;
+							newNode = newNode->ng;
+						}
+						else if (minimum == -1 && visited[newNode->to] == false) {
+							newNode = newNode->ng;
+						}
+						else if (minimum > newNode->dis && visited[newNode->to] == true) {
+							minimum = newNode->dis;
+							endEdge = newNode->to;
+							startEdge = i;
+						}
+						else {
+							newNode = newNode->ng;
+						}
+					}
+				}
+			}
+
+			visited[endEdge] = false;
+			AddEdgeNode(outcome, startEdge, endEdge, minimum);
+
+		} while (everyNodeVisited(visited, size));
+
+		return outcome;
+	}
+	
+	void print_ln(NodeNeigh** lnoriginal, int size) {
+		NodeNeigh** LN = lnoriginal;
+		for (int i = 0; i < size; i++) {
+			std::cout << i << "-> ";
+			while (LN[i]) {
+				std::cout << "to:" << LN[i]->to << " dis:" << LN[i]->dis << "\t";
+				LN[i] = LN[i]->ng;
+			}
+			std::cout << std::endl;
+		}
+	}
+	void print_le(NodeEdge* leoriginal) {
+		NodeEdge* LE = leoriginal;
+		while (LE) {
+			std::cout << "from:" << LE->from << " to:" << LE->to << " dis:" << LE->dis << "-->";
+			LE = LE->ng;
+		}
+		std::cout << "NULL" << std::endl;
+	}
 }
--- Cpp/Main.cpp
+++ Cpp/Main.cpp
@@ -11,31 +11,73 @@
 	std::cout << "----------------------------------------------" << std::endl;
 #define RAND\
 	rand()%99+0
-#define HUGE_ARR 1000
+#define HUGE_ARR 1000000
 
-//using sll::Node;
-//using gr::Node;
+int size = 0;
+void _size() {
+	std::cout << size << std::endl;
+}
 
 void printArray(int arr[], int size);
-void printMatrix(int* arr, int size);
+void printMatrix(int** arr, int size);
+void test_linkedListBasics();
+void test_merge_linkedList();
+void test_mergeSort_linkedLis();
+void test_combSort_linkedLis();
+void test_bubbleSort_linkedLis();
+void test_timeMeasurement_array();
+void test_heapSort_array();
+void test_mergeSort_array();
+void test_matrix_graph();
+void test_ln_graph();
+void test_le_graph();
+void test_graphs();
+void test_Prim();
+
 
 int main()
-{
-	/*srand(time(NULL));
-	Node* head = nullptr;
+{	
+	srand(time(NULL));
+	_size();
+	UNDERLINE;
 
-	for (int i = 0; i < 15; i++) 
+	test_Prim();
+
+	return 0;
+}
+
+void printArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
+
+void printMatrix(int** arr, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++)
+			std::cout << arr[i][j] << "	  ";
+		std::cout << std::endl;
+	}
+}
+
+void test_linkedListBasics()
+{
+	sll::Node* head = nullptr;
+
+	for (int i = 0; i < 15; i++)
 		sll::addFront(head, RAND);
 	sll::show(head);
 	sll::removeBack(head);
 	sll::removeFront(head);
 	sll::show(head);
-	
-	UNDERLINE;
+}
 
-	Node* toMerge1 = nullptr;
-	Node* toMerge2 = nullptr;
-	Node* empty = nullptr;
+void test_merge_linkedList()
+{
+	sll::Node* toMerge1 = nullptr;
+	sll::Node* toMerge2 = nullptr;
+	sll::Node* empty = nullptr;
 	int j = 2;
 	int k = 1;
 	for (int i = 0; i < 10; i++) {
@@ -48,50 +90,41 @@ int main()
 	sll::show(toMerge2);
 	sll::merge(empty, toMerge1, toMerge2);
 	sll::show(empty);
+}
 
-	UNDERLINE;
-
-	Node* toMergeSort = nullptr;
+void test_mergeSort_linkedLis()
+{
+	sll::Node* toMergeSort = nullptr;
 	for (int i = 0; i < 15; i++)
 		sll::addFront(toMergeSort, RAND);
 	std::cout << "Before mergeSort: ";  sll::show(toMergeSort);
 	sa::ll::mergeSort(toMergeSort);
-	std::cout << "After mergeSort: "; sll::show(toMergeSort);*/
+	std::cout << "After mergeSort: "; sll::show(toMergeSort);
+}
 
-	/*UNDERLINE;
-
-	Node* toCombSort = nullptr;
+void test_combSort_linkedLis()
+{
+	sll::Node* toCombSort = nullptr;
 	for (int i = 0; i < 15; i++)
 		sll::addFront(toCombSort, RAND);
 	std::cout << "Before combSort: ";  sll::show(toCombSort);
 	sa::ll::combSort(toCombSort);
 	std::cout << "After combSort: "; sll::show(toCombSort);
+}
 
-	UNDERLINE;
-
-	Node* toBubbleSort = nullptr;
+void test_bubbleSort_linkedLis()
+{
+	sll::Node* toBubbleSort = nullptr;
 	for (int i = 0; i < 15; i++)
 		sll::addFront(toBubbleSort, RAND);
 	std::cout << "Before bubbleSort: ";  sll::show(toBubbleSort);
 	sa::ll::bubbleSort(toBubbleSort);
-	std::cout << "After bubbleSort: "; sll::show(toBubbleSort);*/
+	std::cout << "After bubbleSort: "; sll::show(toBubbleSort);
+}
 
-	UNDERLINE;
-
-	/*const int arrSize = 15;
-	int heapArray[arrSize+1];
-	for (int i = 1; i <= arrSize; i++)
-		heapArray[i] = RAND;
-	std::cout << "Before: ";	for (int i = 1; i <= arrSize; i++)
-									std::cout << heapArray[i] << " ";
-	
-	sa::arr::heapSort(heapArray, arrSize);
-	std::cout << std::endl << "After: "; for (int i = 1; i <= arrSize; i++)
-									std::cout << heapArray[i] << " "; std::cout << std::endl;*/
-
-	UNDERLINE;
-
-	/*long* bigArr1 = new long[HUGE_ARR];
+void test_timeMeasurement_array()
+{
+	long* bigArr1 = new long[HUGE_ARR];
 	long* bigArr2 = new long[HUGE_ARR];
 	long* bigArr3 = new long[HUGE_ARR];
 	long val;
@@ -101,9 +134,9 @@ int main()
 		bigArr2[i] = val;
 		bigArr3[i] = val;
 	}
-	
+
 	std::cout << "Czas dzialania: " << std::endl;
-	
+
 	auto t1 = std::chrono::high_resolution_clock::now();
 	sa::arr::quickSort(bigArr1, 0, HUGE_ARR - 1);
 	auto t2 = std::chrono::high_resolution_clock::now();
@@ -117,63 +150,84 @@ int main()
 	std::cout << "QSH: " << durationQsh << std::endl;
 
 	t1 = std::chrono::high_resolution_clock::now();
-	sa::arr::bubbleSortOptimized(bigArr3, HUGE_ARR - 1);
+	sa::arr::bubbleSortOptimized(bigArr3, 0, HUGE_ARR-1);
 	t2 = std::chrono::high_resolution_clock::now();
 	auto durationBs = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 	std::cout << "BS:  " << durationBs << std::endl;
 
-	delete [] bigArr1;
-	delete [] bigArr2;
-	delete [] bigArr3;*/
+	delete[] bigArr1;
+	delete[] bigArr2;
+	delete[] bigArr3;
+}
 
-	//int tablica[10];
-	//for (int i = 0; i < 10; i++) {
-	//	tablica[i] = RAND;
-	//	//tablica[i] = i;
-	//}
-	//printArray(tablica, 10);
-	//sa::arr::mergeSort<int>(tablica, 10);
-	//printArray(tablica, 10);
+void test_heapSort_array()
+{
+	const int arrSize = 15;
+	int heapArray[arrSize + 1];
+	for (int i = 1; i <= arrSize; i++)
+		heapArray[i] = RAND;
+	std::cout << "Before: ";	for (int i = 1; i <= arrSize; i++)
+		std::cout << heapArray[i] << " ";
 
-	int size;
+	sa::arr::heapSort(heapArray, arrSize);
+	std::cout << std::endl << "After: "; for (int i = 1; i <= arrSize; i++)
+		std::cout << heapArray[i] << " "; std::cout << std::endl;
+}
+
+void test_mergeSort_array()
+{
+	int tablica[10];
+	for (int i = 0; i < 10; i++) {
+		tablica[i] = RAND;
+		//tablica[i] = i;
+	}
+	printArray(tablica, 10);
+	sa::arr::mergeSort<int>(tablica, 10);
+	printArray(tablica, 10);
+}
+
+void test_matrix_graph()
+{
 	int** matrix = gr::createMatrix(size);
+	printMatrix(matrix, size);
+
 	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-			std::cout << matrix[i][j] << "	";
-		std::cout << std::endl;
-	}
-
-	UNDERLINE;
-
-	gr::Node** LN = gr::createLN(size);
-	for (int i = 0; i < size; i++){
-		std::cout << i << "-> ";
-		while(LN[i]->ng != nullptr){
-			std::cout << "to:" << LN[i]->to << "dis:" << LN[i]->dis;
-		}
-		std::cout << std::endl;
-	}
-
-	UNDERLINE;
-
+		delete[] matrix[i];
 	delete[] matrix;
+}
+
+void test_ln_graph()
+{
+	gr::NodeNeigh** LN = gr::createListOfNeighbourhood(size);
+	gr::print_ln(LN, size);
+
+	for (int i = 0; i < size; i++)
+		delete[] LN[i];
 	delete[] LN;
 
-	return 0;
 }
 
-void printArray(int arr[], int size)
+void test_le_graph()
 {
-	for (int i = 0; i < size; i++)
-		std::cout << arr[i] << " ";
-	std::cout << std::endl;
+	gr::NodeEdge* LE = gr::createListOfEdges(size);
+	gr::print_le(LE);
+
+	delete[] LE;
 }
 
-//void printMatrix(int** arr, int size) {
-//	for (int i = 0; i < size; i++) {
-//		for (int j = 0; j < size; j++)
-//			std::cout << arr[i][j] << "	  ";
-//		std::cout << std::endl;
-//	}
-//}
+void test_graphs()
+{
+	test_matrix_graph();
+	UNDERLINE;
+	test_ln_graph();
+	UNDERLINE;
+	test_le_graph();
+
+}
+
+void test_Prim()
+{
+	gr::NodeNeigh** ln = gr::createListOfNeighbourhood(size);
+	gr::NodeEdge* prim = gr::PrimsAlgorithm(ln, size, 6);
+	gr::print_le(prim);
+}
